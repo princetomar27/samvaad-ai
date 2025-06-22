26ac7d9239bf164f8423f042febe1a812298be33
Prince Tomar <tomarprince443@gmail.com>
2025-06-22 22:05:59 +0530

    3: authentication using better-auth setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,64 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { authClient } from "@/lib/auth-client";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export default function Home() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+
+  const { data: session } = authClient.useSession();
+
+  const handleSignUp = async () => {
+    authClient.signUp.email(
+      {
+        email,
+        name,
+        password,
+      },
+      {
+        onError: () => {
+          window.alert("Error signing up");
+        },
+        onSuccess: () => {
+          window.alert("Sign up successful");
+        },
+      }
+    );
+  };
+
+  if (session) {
+    return (
+      <div className="flex flex-col p-8 gap-y-4">
+        <h2>Logged in</h2>
+        <Button onClick={() => authClient.signOut()}>Sign out</Button>
+      </div>
+    );
+  }
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <Button>Click me</Button>
+    <div className="flex flex-col items-center justify-center h-screen gap-4 p-4">
+      <Input
+        placeholder="name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <Input
+        placeholder="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <Input
+        placeholder="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <Button onClick={handleSignUp} className=" hover:cursor-pointer">
+        Sign up
+      </Button>
     </div>
   );
 }
