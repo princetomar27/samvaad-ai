45ed8eed04a9d0623eecbcf0bb4f1b144d525cd8
Prince Tomar <tomarprince443@gmail.com>
2025-06-23 11:06:43 +0530

    4: authentication view and ui done

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "@/components/ui/card";
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+  return (
+    <div>
+      <SignUpView />
+    </div>
+  );
+};
+
+export default Page;
---
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -0,0 +1,222 @@
+"use client";
+
+import { Card, CardContent } from "@/components/ui/card";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { Alert, AlertTitle } from "@/components/ui/alert";
+import { OctagonAlertIcon } from "lucide-react";
+import { useState } from "react";
+import { authClient } from "@/lib/auth-client";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+
+const signUpFormSchema = z
+  .object({
+    name: z.string().min(3, { message: "Name is required" }),
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" }),
+    confirmPassword: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
+export const SignUpView = () => {
+  const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const [pending, setPending] = useState(false);
+  const signUpForm = useForm<z.infer<typeof signUpFormSchema>>({
+    resolver: zodResolver(signUpFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+      name: "",
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signUpFormSchema>) => {
+    setError(null);
+    setPending(true);
+
+    authClient.signUp.email(
+      {
+        email: data.email,
+        password: data.password,
+        name: data.name,
+      },
+      {
+        onSuccess: () => {
+          setPending(false);
+          router.push("/");
+        },
+        onError: ({ error }) => {
+          setPending(false);
+          router.push("/");
+        },
+      }
+    );
+  };
+
+  return (
+    <div className="flex flex-col gap-6 ">
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <Form {...signUpForm}>
+            <form
+              className="p-6 md:p-8"
+              onSubmit={signUpForm.handleSubmit(onSubmit)}
+            >
+              <div className=" flex flex-col gap-6">
+                <div className=" flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Let&apos;s Get Started</h1>
+                  <p className="text-muted-foreground text-balance">
+                    Create your account to continue
+                  </p>
+                </div>
+                <div className=" grid gap-3">
+                  <FormField
+                    control={signUpForm.control}
+                    name="name"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Name</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="name"
+                            placeholder="Prince Tomar"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={signUpForm.control}
+                    name="email"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Email</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="email"
+                            placeholder="abc@example.com"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={signUpForm.control}
+                    name="password"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                  <FormField
+                    control={signUpForm.control}
+                    name="confirmPassword"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormLabel>Confirm Password</FormLabel>
+                        <FormControl>
+                          <Input
+                            type="password"
+                            placeholder="********"
+                            {...field}
+                          />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </div>
+                {error && (
+                  <Alert className="bg-destructive/10 border-none">
+                    <OctagonAlertIcon className="h-4 w-4 !text-destructive" />
+                    <AlertTitle>{error}</AlertTitle>
+                  </Alert>
+                )}
+                <Button
+                  type="submit"
+                  className="w-full hover:cursor-pointer"
+                  disabled={pending}
+                >
+                  Sign Up
+                </Button>
+                <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                  <span className="bg-card text-muted-foreground relative z-10 px-2 ">
+                    Or Continue with
+                  </span>
+                </div>
+                <div className="grid grid-cols-2 gap-4">
+                  <Button variant="outline" className="w-full">
+                    Google
+                  </Button>
+                  <Button variant="outline" className="w-full">
+                    Github
+                  </Button>
+                </div>
+                <div className="text-center text-sm">
+                  Already have an account?{" "}
+                  <Link
+                    href="/sign-in"
+                    className="underline underline-offset-4"
+                  >
+                    Sign In
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </Form>
+          <div className="bg-radial from-purple-300 to-purple-800 relative hidden md:flex flex-col gap-y-4 items-center justify-center h-full">
+            <img src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
+            <p className=" text-2xl font-semibold text-white">Samvaad.AI</p>
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+        <p>
+          By continuing, you agree to our{" "}
+          <a href="#" className="underline underline-offset-4">
+            Terms of Service
+          </a>{" "}
+          and{" "}
+          <a href="#" className="underline underline-offset-4">
+            Privacy Policy
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+};
