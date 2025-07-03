1d317766c525ee6c1651d2eefa858892a0ef39e6
Prince Tomar <tomarprince443@gmail.com>
2025-07-03 16:11:36 +0530

    Enhance MeetingsPage with loading and error states; integrate Suspense and ErrorBoundary for improved user experience during data fetching.

--- src/app/(dashboard)/meetings/page.tsx
+++ src/app/(dashboard)/meetings/page.tsx
@@ -1,11 +1,26 @@
-import { MeetingsView } from "@/modules/meetings/ui/views/meetings-view";
-import React from "react";
+import {
+  MeetingsView,
+  MeetingsViewError,
+  MeetingsViewLoading,
+} from "@/modules/meetings/ui/views/meetings-view";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { HydrationBoundary } from "@tanstack/react-query";
+import React, { Suspense } from "react";
+import { ErrorBoundary } from "react-error-boundary";
 
 const MeetingsPage = () => {
+  const queryClient = getQueryClient();
+
+  void queryClient.prefetchQuery(trpc.meetings.getMany.queryOptions({}));
+
   return (
-    <div>
-      <MeetingsView />
-    </div>
+    <HydrationBoundary>
+      <Suspense fallback={<MeetingsViewLoading />}>
+        <ErrorBoundary fallback={<MeetingsViewError />}>
+          <MeetingsView />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrationBoundary>
   );
 };
 
--- src/modules/meetings/ui/views/meetings-view.tsx
+++ src/modules/meetings/ui/views/meetings-view.tsx
@@ -1,12 +1,32 @@
 "use client";
 
+import { ErrorState } from "@/components/error-state";
+import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
-import { useQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import React from "react";
 
 export const MeetingsView = () => {
   const trpc = useTRPC();
-  const { data } = useQuery(trpc.meetings.getMany.queryOptions({}));
+  const { data } = useSuspenseQuery(trpc.meetings.getMany.queryOptions({}));
 
   return <div>{JSON.stringify(data?.items)}</div>;
+};
+
+export const MeetingsViewLoading = () => {
+  return (
+    <LoadingState
+      title="Loading meetings"
+      description="Please wait while we load the meetings"
+    />
+  );
+};
+
+export const MeetingsViewError = () => {
+  return (
+    <ErrorState
+      title="Error loading meetings"
+      description="Please try again later"
+    />
+  );
 };
