6bb86cafc712bb7db52b4e3006ab088b47d3a41e
Prince Tomar <tomarprince443@gmail.com>
2025-09-11 00:47:43 +0530

    Update SignInView component to use width and height props for Image

--- src/modules/auth/ui/views/sign-in-view.tsx
+++ src/modules/auth/ui/views/sign-in-view.tsx
@@ -184,7 +184,13 @@ export const SignInView = () => {
             </form>
           </Form>
           <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 items-center justify-center h-full">
-            <Image src="/logo.svg" alt="Image" className="h-[92px] w-[92px]" />
+            <Image
+              src="/logo.svg"
+              alt="Image"
+              width={92}
+              height={92}
+              className="h-[92px] w-[92px]"
+            />
             <p className=" text-2xl font-semibold text-white">samvaad.ai</p>
           </div>
         </CardContent>
